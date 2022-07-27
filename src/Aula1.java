86115620fb06049b04a7bbba3fd46128e2495fc1
AnaLuizaAT <aluiza.araujot@gmail.com>
2022-07-26 22:07:47 -0400

    Aula 1

---
+++ src/Aula1.java
@@ -0,0 +1,13 @@
+public class Aula1 {
+    private int j; //ATRIBUTO
+
+    public static void main(String[] args) {
+        int i = 0; //VAR DO METODO MAIN
+    }
+    Cachorro doguinho = new Cachorro(); //OBJETO DOGUINHO
+
+    private void soma(int a, int b) {
+        int i = 2; //VAR DO METODO SOMA
+        int soma = a + b + i + j;
+    }
+}
---
+++ src/Cachorro.java
@@ -0,0 +1,10 @@
+public class Cachorro {
+    public class Cachorro {
+        private String nome; //POR SER LETRA MAIUSCULA SE TORNA UMA CLASSE
+        private int idade; //VARIAVEL DO TIPO PRIMITIVO
+        private String raca; //CLASSE
+        private boolean dono;
+        private boolean castrado;
+        private float peso;
+    }
+}
