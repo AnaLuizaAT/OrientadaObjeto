e6d32eb2f50e4c7d1a28caa8be1952e3b082dd47
AnaLuizaAT <aluiza.araujot@gmail.com>
2022-07-27 21:52:00 -0400

    Aula 2

--- src/Aula1.java
+++ src/Aula1.java
@@ -1,13 +1,36 @@
+//
+
+// Classes devem ter a primeira letra maiúscula e as outras minúsculas
 public class Aula1 {
-    private int j; //ATRIBUTO
 
     public static void main(String[] args) {
-        int i = 0; //VAR DO METODO MAIN
-    }
-    Cachorro doguinho = new Cachorro(); //OBJETO DOGUINHO
+        // Estou criando um onjeto do tipo Cachorro, chamado doguinho
+        // Estou instanciado através do operador new
+        // Após o new eu chamo o construtor da classe
+        Cachorro doguinho = new Cachorro("doguinho");
 
-    private void soma(int a, int b) {
-        int i = 2; //VAR DO METODO SOMA
-        int soma = a + b + i + j;
+        doguinho.setNome("Rex");
+        doguinho.setIdade(7);
+        doguinho.setRaca("Caramelo");
+        doguinho.setDono(true);
+        doguinho.setCastrado(false);
+        doguinho.setPeso(7.3);
+
+        // System.out.println(doguinho.getNome());
+        // System.out.println(doguinho.getIdade());
+        // System.out.println(doguinho.getRaca());
+        // System.out.println(doguinho.getDono());
+        // System.out.println(doguinho.getCastrado());
+        // System.out.println(doguinho.getPeso());
+
+        System.out.println(doguinho.toString());
+
+        // Cachorro toto = new Cachorro();
+        // toto.setNome("Thor");
+        // toto.setIdade(9);
+        // toto.setRaca("Pit Bull");
+        // toto.setDono(false);
+        // toto.setCastrado(true);
+        // toto.setPeso(11.8);
     }
 }
--- src/Cachorro.java
+++ src/Cachorro.java
@@ -1,10 +1,83 @@
 public class Cachorro {
-    public class Cachorro {
-        private String nome; //POR SER LETRA MAIUSCULA SE TORNA UMA CLASSE
-        private int idade; //VARIAVEL DO TIPO PRIMITIVO
-        private String raca; //CLASSE
-        private boolean dono;
-        private boolean castrado;
-        private float peso;
+
+    //ATRIBUTOS
+    private String nome; //POR SER LETRA MAIUSCULA SE TORNA UMA CLASSE
+    private int idade; //VARIAVEL DO TIPO PRIMITIVO
+    private String raca; //CLASSE
+    private boolean dono;
+    private boolean castrado;
+    private double peso;
+
+    public Cachorro() {
+    }
+
+    //CONSTRUTORES
+    public Cachorro(String nome) {
+        System.out.println("Criando o objeto: " + nome);
+    }
+
+    //Getters e Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRaca(String raca) {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public boolean isDono() {
+        return dono;
+    }
+
+    public void setDono(boolean dono) {
+        this.dono = dono;
+    }
+
+    public boolean isCastrado() {
+        return castrado;
+    }
+
+    public void setCastrado(boolean castrado) {
+        this.castrado = castrado;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    // MÉTODOS
+    public String toString() {
+        return ("Nome: " +
+                this.nome +
+                "\nIdade: " +
+                this.idade +
+                "\nRaça: " +
+                this.raca +
+                "\nDono: " +
+                this.dono +
+                "\nCastrado: " +
+                this.castrado +
+                "\nPeso: " +
+                this.peso);
     }
 }
