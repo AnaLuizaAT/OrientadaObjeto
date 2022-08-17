05399719651c11dd7fc57369a28d0382869b9db5
AnaLuizaAT <aluiza.araujot@gmail.com>
2022-08-17 19:15:15 -0400

    Aula 6

--- src/poo/exercicioHeranca/sistemaBiblioteca.java
+++ src/poo/exercicioHeranca/sistemaBiblioteca.java
@@ -11,15 +11,15 @@ public class sistemaBiblioteca {
         List<Autor> autores = new ArrayList<Autor>();
         List<Livro> livros = new ArrayList<Livro>();
 
-        do {
-            System.out.println("Sistema biblioteca \n" +
-                    "Escolha uma opção: \n" +
-                    "1 - Cadastrar Livro \n" +
-                    "2- Cadastrar Funcionario \n" +
-                    "3- Cadastrar Estudante \n" +
-                    "4- Cadastrar Autor \n" +
-                    "5- Realizar Emprestimo \n" +
-                    "6- Receber Emprestimo\n");
-        } while (op != 0);
+        String[] opcoes = {
+                "Sistema biblioteca",
+                "Escolha uma opção:",
+                "1 - Cadastrar Livro",
+                "2- Cadastrar Funcionario",
+                "3- Cadastrar Estudante",
+                "4- Cadastrar Autor",
+                "5- Realizar Emprestimo",
+                "6- Receber Emprestimo",
+        };
     }
 }
